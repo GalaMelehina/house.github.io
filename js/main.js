dd2f5a39ffbada779e180f69abeabab276abd9e8
Галя Мелехина <gala_red@mail.ru>
2022-04-28 20:55:52 +0500

    + mobile version

--- js/main.js
+++ js/main.js
@@ -51,5 +51,26 @@ prxScene.addEventListener('mousemove', function (e) {
     }
 });
 
+const navIcon = document.querySelector('.nav-icon');
+const nav = document.querySelector('.main__nav');
+
+navIcon.addEventListener('click', function () {
+	this.classList.toggle('nav-icon--active');
+	nav.classList.toggle('nav--active');
 });
+
+// Находим ссылки внутри мобильной навигации
+const navLinks = document.querySelectorAll('.main__nav-item');
+
+// Обходим ссылки методом forEach
+navLinks.forEach(function (item) {
+	// Для каждой ссылки добавляем прослушку по событию "Клик"
+	item.addEventListener('click', function () {
+		navIcon.classList.remove('nav-icon--active'); // Убираем активный класс у иконки моб. навигации
+		nav.classList.remove('nav--active'); // Убираем активный класс у блока моб. навигации
+	})
+});
+
+});
+
 
